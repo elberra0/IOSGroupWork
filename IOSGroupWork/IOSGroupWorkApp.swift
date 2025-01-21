99b1484018af765a57417f36a2221f408c2e541f
alejandroberraondosoria <elberra0@gmail.com>
2025-01-21 13:05:54 +0100

    Initial Commit

---
+++ IOSGroupWork/IOSGroupWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSGroupWorkApp.swift
+//  IOSGroupWork
+//
+//  Created by Alejandro Berraondo Soria on 21/1/25.
+//
+
+import SwiftUI
+
+@main
+struct IOSGroupWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
